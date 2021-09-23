3cc67b695f707db1f2acd9d15f2f76bdb73956e8
Loxpex <90549537+Loxpex@users.noreply.github.com>
2021-09-23 12:31:25 +0500

    Program has been added

---
+++ TheCaesarSipher/TheCaesarSipher/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace TheCaesarCipher
+{
+    class Program
+    {
+        static string alphabet = "abcdefghijklmnopqrstuvwxyz";
+        static void Main()
+        {
+            int Shift;
+            Console.WriteLine("Введите строку");
+            string text = Console.ReadLine();
+        Back:
+            Console.WriteLine("Введите сдвиг строки");
+            try
+            {
+                Shift = Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Вы ввели не число");
+                Console.ReadKey();
+                Console.Clear();
+                goto Back;
+            }
+
+
+            char[] words = text.ToCharArray();
+            foreach (char s in words)
+            {
+                int indexOfChar = alphabet.IndexOf(s);
+
+                if (indexOfChar == -1)
+                {
+                    Console.Write(" ");
+                }
+                else if (indexOfChar + Shift < 26)
+                {
+                    indexOfChar += Shift;
+                    Console.Write(alphabet[indexOfChar]);
+                }
+                else
+                {
+                    indexOfChar = indexOfChar + Shift - 26;
+                    Console.Write(alphabet[indexOfChar]);
+
+                }
+            }
+        }
+    }
+}
